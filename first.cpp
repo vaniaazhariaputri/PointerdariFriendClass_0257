f512c5a1a3846f3f00848d5f62946aa558931398
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-06 10:36:24 +0700

    Menambahkan void input

--- first.cpp
+++ first.cpp
@@ -7,4 +7,20 @@ public // akses modifier
     int panjang, lebar;
 
 public:
-}
+
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran ol;
+
+};
