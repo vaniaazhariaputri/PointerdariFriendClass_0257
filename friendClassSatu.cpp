8851bc96d8425a1a2b52aba1921666f71c5bf9a0
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-06 11:35:16 +0700

    Membuat class siswa dan orang , membuat modifier

---
+++ friendClassSatu.cpp
@@ -0,0 +1,14 @@
+#incluide <iostream>
+3include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
