210eba1228d107c081a8ea41b11faadcb955eea9
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-06 11:22:43 +0700

    Memasukkan fungsi nama mahasiswa dan set nama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -10,3 +10,16 @@ private:
 public:
     friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko. "Kairi Kumar");
+    return 0;
+}
