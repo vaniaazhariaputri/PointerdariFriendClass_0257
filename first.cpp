a48b6818827c2f7c1bac991ada91f946073409b2
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-06 10:46:17 +0700

    Menambahkan void input jari jari dan luas lingkaran

--- first.cpp
+++ first.cpp
@@ -30,4 +30,16 @@ class Lingkaran
 public:
     int jarijari;
 
-}
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin >> jarijari;
+    }
+
+    int luaslingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
+
+
