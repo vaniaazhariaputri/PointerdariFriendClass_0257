b49dc6c3b75ad864d749f3c55ca66ce8e2a8a810
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-06 11:05:54 +0700

    Membuat fungsi pointer, object mahasiswa

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,18 @@ class mahasiswa{
         cout<<"No Induk = "<<nim<<endl;
     }
 };
+
+int main (){
+
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //Member Acces Operator 
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Acces Operator
+    mhs.ShowNim();
+
+    mahasiswa *pMhs = &mhs; //pointer bereference pMhs 
+    pMhs->nim = 3; //Arrow Operator 
+    pMhs->ShowNim();
+    return 0;
+}
