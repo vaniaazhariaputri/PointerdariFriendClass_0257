a0253405294c4a980d836b3d8baa3529dec13a5e
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-06 11:39:17 +0700

    Membuat proedur ID

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -12,3 +12,13 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAII(orang &a);
+};
