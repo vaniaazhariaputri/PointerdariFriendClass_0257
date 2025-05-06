3813d076becd1a811cd3cb50bbdaa1cceb1208e1
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-06 11:29:51 +0700

    Memasukkan void manusia

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -7,3 +7,15 @@ class manusia
 public:
     void showNilaiPelajar(pelajar &x);
 };
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar(). { nilai = 100; }
+    friend void manusia::ShowNilaiPelajar(pelajar &x);
+};
+
+void manusia::ShowNilaiPelajar(pelajar &x)
